b38bbfe4d59ee2c83b567e88ea7884f2389a0e49
Idrissa <idrisabkar2018@gmail.com>
2023-12-08 11:57:39 +0300

    about page ui/responsive done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,24 @@
 function About() {
   return (
-    <div>
-      <h1>about</h1>
+    <div className="flex gap-9 flex-col items-center justify-center">
+      <>
+        <h1 className="text-xl tracking-wider font-bold text-primary-text lg:text-6xl">
+          We Love{' '}
+          <span
+            className="p-2 bg-tertiary-100
+text-btn-100 rounded-xl"
+          >
+            RusoFy
+          </span>
+        </h1>
+        <span className="text-primary-text lg:leading-8 leading-8 lg:text-base text-sm  md:px-[10%]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quae
+          quam blanditiis vitae, dolor non eveniet ipsum voluptatibus, quia
+          optio aut! Perferendis ipsa cumque ipsam nostrum reprehenderit ad illo
+          sed officiis ea tempore! Similique eos minima sit porro, ratione
+          aspernatur!
+        </span>
+      </>
     </div>
   )
 }
