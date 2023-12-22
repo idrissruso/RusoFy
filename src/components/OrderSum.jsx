9c88594abc85a1939807bed4b54aa754672b580a
Idrissa <idrisabkar2018@gmail.com>
2023-12-22 17:51:45 +0300

    quelques t-retouches

--- src/components/OrderSum.jsx
+++ src/components/OrderSum.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux'
+
 function OrderSum() {
+  const { totalPrice, shippingFee,tax } = useSelector((state) => state.cartSlice)
   return (
     <div className="flex flex-col gap-2 bg-primary-600 min-w-[20rem] px-7 py-8 rounded-2xl text-xs text-primary-text drop-shadow-xl max-h-[13rem]">
-      <Row title="Subtotal" value="$ 100" />
-      <Row title="Shipping" value="$ 10" />
-      <Row title="Tax" value="$ 10" />
+      <Row title="Subtotal" value={totalPrice} />
+      <Row title="Shipping" value={shippingFee} />
+      <Row title="Tax" value={tax} />
       <div className="text-sm mt-3">
-        <Row title="Total" value="$ 120" total={true} />
+        <Row
+          title="Total"
+          value={totalPrice + shippingFee + tax}
+          total={true}
+        />
       </div>
     </div>
   )
